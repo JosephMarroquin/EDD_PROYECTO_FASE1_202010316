15d85fae6d479ccfc840afebeb071685c320841c
josep <josep@DESKTOP-5ND3OEA>
2022-02-04 00:38:39 -0600

    menu principal en consola

--- [EDD]PROYECTO_FASE1_202010316/src/edd/proyecto_fase1_202010316/EDDPROYECTO_FASE1_202010316.java
+++ [EDD]PROYECTO_FASE1_202010316/src/edd/proyecto_fase1_202010316/EDDPROYECTO_FASE1_202010316.java
@@ -5,6 +5,9 @@
  */
 package edd.proyecto_fase1_202010316;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author josep
@@ -16,7 +19,103 @@ public class EDDPROYECTO_FASE1_202010316 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Prueba comit");
+        
+        Scanner sn= new Scanner(System.in);
+        boolean salir=false;
+        int opcion;
+        
+        while(!salir){
+            
+            System.out.println("-------------------------------------------");
+            System.out.println("| 1. Parámetros iniciales                 |");
+            System.out.println("| 2. Ejecutar paso                        |");
+            System.out.println("| 3. Estado en memoria de las estructuras |");
+            System.out.println("| 4. Reportes                             |");
+            System.out.println("| 5. Acerca de                            |");
+            System.out.println("| 6. Salir                                |");
+            System.out.println("-------------------------------------------");
+            
+            try{
+            
+                System.out.println("Introduce una opcion");
+                opcion=sn.nextInt();
+
+                switch(opcion){
+                    case 1:
+                        parametrosIniciales();
+                        break;
+                    case 2:
+                        System.out.println("opcion 2");
+                        break;
+                    case 3:
+                        System.out.println("opcion 3");
+                        break;
+                    case 4:
+                        System.out.println("opcion 4");
+                        break;
+                    case 5:
+                        System.out.println("--------DATOS DEL ESTUDIANTE--------");
+                        System.out.println("| Joseph Jeferson Marroquín Monroy |");
+                        System.out.println("| 202010316                        |");
+                        System.out.println("| 5to semestre                     |");
+                        System.out.println("| Proyecto Fase 1                  |");
+                        System.out.println("| Estructura de datos Seccion C    |");
+                        System.out.println("------------------------------------");
+                        break;
+                    case 6:
+                        salir=true;
+                        break;
+                    default:
+                        System.out.println("Ingrese una opcion valida");
+                }
+            
+            }catch(InputMismatchException e){
+                System.out.println("Opcion Invalida");
+                sn.next();
+            }
+            
+        }
+        
+    }
+    
+    public static void parametrosIniciales(){
+        Scanner sn= new Scanner(System.in);
+        boolean salir=false;
+        String opcion=" ";
+        
+        while(!salir){
+            
+            System.out.println("-------------------------------------------");
+            System.out.println("| a. Carga masiva de clientes             |");
+            System.out.println("| b. Cantidad de ventanillas              |");
+            System.out.println("| c. Regresar                             |");
+            System.out.println("-------------------------------------------");
+            
+            try{
+            
+                System.out.println("Introduce una opcion");
+                opcion=sn.nextLine();
+
+                switch(opcion){
+                    case "a":
+                        System.out.println("opcion a");
+                        break;
+                    case "b":
+                        System.out.println("opcion b");
+                        break;
+                    case "c":
+                        salir=true;
+                        break;
+                    default:
+                        System.out.println("Ingrese una opcion valida");
+                }
+            
+            }catch(InputMismatchException e){
+                System.out.println("Opcion Invalida");
+                sn.next();
+            }
+            
+        }
     }
     
 }
